2f634b6c680609779ad8ec77445550927a81d5c6
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-03-30 20:10:58 +0400

    Add solution and core console project

---
+++ MIC.Volo.FibSequence.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MIC.Volo.FibSequence.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
