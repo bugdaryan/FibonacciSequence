63d09f181f336d5cfb854a30abac33bbf441c1b6
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-03-30 20:30:57 +0400

    Add getting Fibonacci sequence usage in Main

--- MIC.Volo.FibSequence.ConsoleApp/Program.cs
+++ MIC.Volo.FibSequence.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace MIC.Volo.FibSequence.ConsoleApp
@@ -8,10 +9,36 @@ namespace MIC.Volo.FibSequence.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            int count = 100;
+            int start = 0;
+            if (args.Length == 1)
+            {
+                int.TryParse(args[0], out count);
+            }
+            else if (args.Length == 2)
+            {
+                int.TryParse(args[0], out start);
+                int.TryParse(args[1], out count);
+            }
+            else if (args.Length > 2)
+            {
+                throw new ArgumentException("Invalid number of arguments given", nameof(args));
+            }
+
+            start = Math.Max(start - 1, 0);
+            var fibs = FibonacciSequence();
+            int counter = 1;
+            foreach (var num in fibs.Skip(start).Take(count))
+            {
+                Console.WriteLine($"{start+counter++}: {num}");
+            }
         }
 
-        private static IEnumerable<BigInteger> FibonacciSequence()
+        /// <summary>
+        /// A Function that gets Fibonacci sequence
+        /// </summary>
+        /// <returns>IEnumerable</BigInteger></returns>
+        public static IEnumerable<BigInteger> FibonacciSequence()
         {
             var a = new BigInteger(1);
             var b = new BigInteger(0);
